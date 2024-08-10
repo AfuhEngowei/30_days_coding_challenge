0e0a7ec5757528ea05e0c03e27f82f0b4c878786
Afuh Engowei <ashleyafuh@gmail.com>
2024-08-10 10:05:56 +0100

    updated style

--- day5.php
+++ day5.php
@@ -8,7 +8,74 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Day5</title>
-</head>
+    <style> 
+        
+body{
+    background-color: white;
+    margin:100px;
+    padding: 0px;
+    display: block;
+    justify-content:center;
+    margin-left: 500px;
+    align-items:center;
+}
+form{
+    width: 300px;
+    height: 300px;
+    padding: 20px;
+    background-color: skyblue;
+    border-radius: 20px;
+    margin-top:100px;
+    z-index: 1;
+    justify-content:center;
+
+
+}
+legend{
+    text-align: center;
+    font-size:30px;
+    margin-bottom:  20px;
+    letter-spacing: 0.5px;
+}
+label{
+    display:block;
+    margin-bottom:5px;
+    margin-top:10px;
+    font-size:18px;
+}
+input{
+    width:280px;
+    height:30px;
+    border-radius:10px;
+}
+h3{
+    font-size:20px;
+}
+select{
+    margin-bottom: 10px;
+
+}
+input[type="submit"]{
+    font-size:18px;
+    margin-top: 40px;
+    width : 100px;
+    margin-left: 100px
+
+}
+input[type="submit"]:active{
+    background-color:blue;
+    color:white;
+}
+p{
+    margin-top:20px;
+    z-index: 2;
+    font-size: 20px;
+}
+
+
+
+    </style>
+    </head>
 <body>
     <form action = " day5.php" method = "post">
         <label for= "Guessed number"> Enter any number between 1 and 100</label>
